8fe46fcb0267ce4ba40d64030103a15a6de10018
svzhuk-vn <karbonit1992@gmail.com>
2021-11-15 19:35:53 +0100

    add level 7

--- Assets/Scripts/ItemManager.cs
+++ Assets/Scripts/ItemManager.cs
@@ -13,7 +13,7 @@ public class ItemManager : MonoBehaviour
     void Start()
     {
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-        levelManager = GameObject.Find("LevelManager").GetComponent<LevelManager>();
+        //levelManager = GameObject.Find("LevelManager").GetComponent<LevelManager>();
         endPath = gameManager.endPoint;
     }
 
